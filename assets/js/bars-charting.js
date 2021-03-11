c0078e1591c10d16100474b245865d04a3a5269e
jamesr1775 <james.rooney@ucdconnect.ie>
2021-03-11 17:16:54 +0000

    The bar heights are now randomized, added a sort button

--- assets/js/bars-charting.js
+++ assets/js/bars-charting.js
@@ -6,6 +6,7 @@ function generateBarChart(){
     let barContainer = document.getElementById('bars-container')
     let spacerContainer = '<div class="col-1"></div>'
     let allBars = ""
+    /*Need to remove these magic numbers for screen size adjusting but working for now*/
     for(let i=0; i<(7/10)*(screen.width/30); i++){
         let singleBar = '<div id="bar-' + i + '" class="single-bar"></div>'
         console.log(singleBar)
@@ -15,6 +16,13 @@ function generateBarChart(){
                                 <div class="col-10">${allBars}</div>
                                 ${spacerContainer}`
     barContainer.innerHTML = barsContainerInnerHtml
+    let bars = document.getElementsByClassName("single-bar");
+    console.log('setting new heights: called')
+    for(let bar of bars){
+        /*Need to remove these magic numbers for screen size adjusting but working for now*/
+        let newHeight = Math.ceil(Math.random() * screen.height*0.5)
+        bar.setAttribute("style", "height: " + newHeight + "px")
+    }
 }
 
 
@@ -22,3 +30,7 @@ $('#generateBars').click(function() {
     console.log('generateBars click: called')
     generateBarChart();
 });
+$('#sortBars').click(function() {
+    console.log('sortBars click: called')
+    sortBars();
+})
