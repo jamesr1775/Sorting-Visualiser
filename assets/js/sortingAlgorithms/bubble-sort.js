bb82aabba846280c35e8459e571efd2358a6943f
jamesr1775 <james.rooney@ucdconnect.ie>
2021-04-07 12:22:03 +0000

    Commiting quick sort quiz updated and general styling of algo info/ code block

--- assets/js/sortingAlgorithms/bubble-sort.js
+++ assets/js/sortingAlgorithms/bubble-sort.js
@@ -80,9 +80,10 @@ export function getBubbleSortCodeString(){
 }
 
 export function getBubbleSortInfoString(){
-    let bubbleSortInfoString = `<h2 id="algo-header">Bubble Sort Algorithm</h2>
+    let bubbleSortInfoString = `<h2 id="algo-header" class="text-center">Bubble Sort Algorithm</h2>
                                 <p>Bubble sort is an old and well known algorithm that sorts the array, usually in ascending order.</p>
-                                <h3>Algorithm Steps :</h3>
+                                <div  class="horizontal-divider"><hr/></div>
+                                <h3 class="text-center">Algorithm Steps</h3>
                                 <ol>
                                     <li>It passes through the array of <em>n</em> values, comparing adjacent values to each other.</li>
                                     <li>If this comparison finds values out of order, it will swap the values in place.</li>
@@ -90,7 +91,8 @@ export function getBubbleSortInfoString(){
                                     <li>Repeat this procedure but now only iterating to the <em>n-i</em> positions.</li>
                                     <li>The algorithm will terminate when all the values are in their correct position.</li>
                                 </ol>
-                                <h3>Complexity</h3>
+                                <div  class="horizontal-divider"><hr/></div>
+                                <h3 class="text-center">Complexity</h3>
                                 <p>In the worst case scenario (the list is in reverse order), the inner for loop will run <em>n-1</em> times, 
                                 on the second iteration it will run <em>n-2</em> times. Thus what will the time complexity be? Take the quiz below to find out!</p>
     `
